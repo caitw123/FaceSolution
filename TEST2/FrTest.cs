5a3f763fd4247eaa190e6e6b35386620f479086a
caitw123 <305202057@qq.com>
2018-01-02 16:22:42 +0800

    Signed-off-by: caitw123 <305202057@qq.com> 更新

--- TEST2/FrTest.cs
+++ TEST2/FrTest.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TEST2
+namespace FaceSolution
 {
-    public partial class Form1 : Form
+    public partial class FrTest : Form
     {
-        public Form1()
+        public FrTest()
         {
             InitializeComponent();
         }
